e7e2e0fa906b29965ef2cb8d79dca87067546069
Mango <jdy0120120@gmail.com>
2020-08-16 08:17:55 +0900

    Add files via upload
    2020.08.16

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -31,6 +34,46 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ){
+    // 비밀번호를 암호화 시킨다.
+    var user = this;
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return next(err)
+        
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                // Store hash in your password DB.
+                user.password = hash
+                next()
+            });
+        });
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // plainPassword 1234567    암호화된 비밀번호 $2b$10$D6w65UMTFaUPcJyxrNUqe.M3/IK/CZIzOJs8Um8bqLiPBnIEGVB2W
+    bcrypt.compare(plainPassword,this.password, function(err, isMatch){
+        if(err) return cb(err),
+                cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    
+    
+    user.token = tokenuser.save(function(err,user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+    //jsonwebtoken을 이용해서 
+}
 
 const User = mongoose.model('User',userSchema)
 
